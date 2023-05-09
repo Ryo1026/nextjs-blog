ebf0af3322c5e7de895c5661906919ca7f9acc1a
Ryo1026 <c9pcv5nvbp@gmail.com>
2023-05-09 18:24:10 +0800

    Add css module

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,27 +1,32 @@
 import Head from "next/head";
 import Link from "next/link";
 import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <>
-      <Head>
-        <title>First Post</title>
-        {/* 可以設定 browser tag 的 name */}
-        <script src="" />
-        {/* 可以放一些第三方JS，ex:SDK */}
-        <Script
-          src="https://connect.facebook.net/en_US/sdk.js"
-          strategy="lazyOnload"
-          onLoad={() =>
-            console.log("script loaded correctly, window.FB has been populated")
-          }
-        />
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+          {/* 可以設定 browser tag 的 name */}
+          <script src="" />
+          {/* 可以放一些第三方JS，ex:SDK */}
+          <Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"
+            onLoad={() =>
+              console.log(
+                "script loaded correctly, window.FB has been populated"
+              )
+            }
+          />
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </Layout>
     </>
   );
 }
