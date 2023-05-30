abc93c428190f1ddae9310237dfb2d49805242df
Ryo1026 <c9pcv5nvbp@gmail.com>
2023-05-30 16:53:02 +0800

    Update introduction

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,12 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm Zafer!</p>
+        <p>
+          Hi, my name Zafer! I am a frontend engineer with a passion for
+          creating engaging and user-friendly web experiences. With two years of
+          experience in the industry, I have developed a strong proficiency in
+          HTML, CSS, and JavaScript, as well as framework like React.
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
